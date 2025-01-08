104d710da0c0b719a9f809b593e1936da1bc0e37
nomina-muniz <jesse.muniz@deforris.com>
2025-01-08 15:10:54 -0500

    init commit with the absolute basics of a script to start.

--- init.sh
+++ init.sh
@@ -1,5 +1,17 @@
+#!/bin/bash/
 apt update -y
 apt upgrade -y
 apt autoremove -y
 apt install software-properties-common
+cd ..
+mkdir .ssh
+read -p "Type in username:" username
+ssh-keygen -t ed25519 -C $username -f $(pwd)/.ssh/id_ed25519
+eval "$(ssh-agent -s)"
+ssh-add $(pwd)/.ssh/id_ed25519
+echo Add ssh key to github...
+cat $(pwd)/.ssh/id_ed25519.pub
+echo Use machine name shown below...
+hostname
+echo Press enter when done...
 bash git.sh
