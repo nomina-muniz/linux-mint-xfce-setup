969f287b858613513667ab091d8f5207b2f7059c
nomina-muniz <nomina-muniz@protonmail.com>
2025-01-08 14:38:59 -0500

    init.sh and git.sh with basic additions

---
+++ git.sh
@@ -0,0 +1,3 @@
+git config --global user.name "nomina-muniz"
+git config --global user.email jesse.muniz@deforris.com
+git config --global color.ui auto
---
+++ init.sh
@@ -0,0 +1,5 @@
+apt update -y
+apt upgrade -y
+apt autoremove -y
+apt install software-properties-common
+bash git.sh
