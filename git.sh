e3e6503fd9fa512abf0ab9c97299eaf08c483032
nomina-muniz <jesse.muniz@deforris.com>
2025-01-18 05:38:00 -0500

    changes to setup github cli cause lazy.

--- git.sh
+++ git.sh
@@ -1,3 +1,14 @@
 git config --global user.name "nomina-muniz"
 git config --global user.email jesse.muniz@deforris.com
 git config --global color.ui auto
+
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
+bash nvim.sh
